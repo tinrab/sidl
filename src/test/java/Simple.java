77998293bfac05d6973dc61ec29e6268257a26e3
paidgeek <tin.rabzelj@gmail.com>
2016-09-01 16:41:43 +0200

    java primitive writes

--- src/test/java/Simple.java
+++ src/test/java/Simple.java
@@ -1,110 +1,131 @@
 import java.io.*;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 @SuppressWarnings("unused")
 public final class Simple {
-	private Item item;
 	private String name;
 	private Double value;
 	private Integer[] arr;
-	public static final int ARR_LENGTH = 2;
+	public static final int ARR_LENGTH = 10;
 	private java.util.List<Integer> li;
+
 	public Simple() {
 		reset();
 	}
-	public Simple(Item item, String name, Double value, Integer[] arr, java.util.List<Integer> li) {
-		this.item = item;
+
+	public Simple(String name, Double value, Integer[] arr, java.util.List<Integer> li) {
 		this.name = name;
 		this.value = value;
 		this.arr = arr;
 		this.li = li;
 	}
-	public Item getItem() {
-		return item;
-	}
-	public void setItem(Item item) {
-		this.item = item;
-	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Double getValue() {
 		return value;
 	}
+
 	public void setValue(Double value) {
 		this.value = value;
 	}
+
 	public Integer[] getArr() {
 		return arr;
 	}
+
 	public void setArr(Integer[] arr) {
 		this.arr = arr;
 	}
+
 	public Integer getArrAt(int index) {
 		return this.arr[index];
 	}
+
 	public void setArrAt(int index, Integer value) {
 		this.arr[index] = value;
 	}
+
 	public java.util.List<Integer> getLi() {
 		return li;
 	}
+
 	public void setLi(java.util.List<Integer> li) {
 		this.li = li;
 	}
+
 	public Integer getLiAt(int index) {
 		return this.li.get(index);
 	}
+
 	public void setLiAt(int index, Integer value) {
 		this.li.set(index, value);
 	}
+
 	public void reset() {
-		this.item = null;
 		this.name = "";
 		this.value = 0.0;
 		this.arr = new Integer[ARR_LENGTH];
 		this.li = new ArrayList<Integer>();
 	}
-	public void writeTo(ByteBuffer buf) throws IOException {
-		this.item.writeTo(buf);
-		Bits.writeString(buf, this.name);
-		buf.putDouble(this.value);
-		for (int i = 0; i < ARR_LENGTH; i++) {
-			buf.putInt(this.arr[i]);
+
+	public int writeTo(byte[] buf, int offset) throws IOException {
+		{
+			byte[] utf8 = this.name.getBytes("UTF-8");
+			int length = utf8.length;
+			int i = 0;
+			while ((length & 0xFFFFFF80) != 0L) {
+				buf[offset + 9 - i++] =  ((byte) ((length & 0x7F) | 0x80));
+				length >>>= 7;
+			}
+			buf[offset] = ((byte)(length & 0x7F));
+			offset += i + 1;
+			System.arraycopy(utf8, 0, buf, offset, utf8.length);
+			offset += utf8.length;
 		}
-		Bits.writeUnsignedVarInt(buf, this.li.size());
-		for (int i = 0; i < this.li.size(); i++) {
-			int liValue = this.li.get(i);
-			buf.putInt(liValue);
+		{
+
 		}
+		return offset;
 	}
+
 	private void writeObject(ObjectOutputStream oos) throws IOException {
 	}
-	public void readFrom(ByteBuffer buf) throws IOException {
-		if(item == null) {
-			item = new Item();
+
+	public int readFrom(byte[] buf, int offset) throws IOException {
+		{
+			int value = 0;
+			int i = 0;
+			byte rb = Byte.MIN_VALUE;
+			for (; offset < buf.length; offset++) {
+				byte b = buf[offset];
+				rb = b;
+				if ((b & 0x80) == 0) {
+					offset++;
+					break;
+				}
+				value |= (b & 0x7f) << i;
+				i += 7;
+				if (i > 35) {
+					throw new IllegalArgumentException("Variable length quantity is too long");
+				}
+			}
+			int length = value | (rb << i);
+			byte[] bytes = new byte[length];
+			System.arraycopy(buf, offset, bytes, 0, length);
+			offset += length;
+			this.name = new String(bytes, "UTF-8");
 		}
-		item.readFrom(buf);
-		this.name = Bits.readString(buf);
-		this.value = buf.getDouble();
-		if(this.arr == null) {
-			this.arr = new Integer[ARR_LENGTH];
-		}
-		for (int i = 0; i < ARR_LENGTH; i++) {
-			this.arr[i] = buf.getInt();
-		}
-		int liSize = Bits.readUnsignedVarInt(buf);
-		if(this.li == null) {
-			this.li = new ArrayList<Integer>();
-		}
-		for (int i = 0; i < liSize; i++) {
-			this.li.add(buf.getInt());
-		}
+
+		return offset;
 	}
+
 	private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
 	}
 }
--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -5,35 +5,17 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class StreamTest {
-
-	@Test
-	public void testItem() throws Exception {
-		Item item = new Item("Apple");
-		ByteBuffer bb = ByteBuffer.allocate(1024);
-
-		item.writeTo(bb);
-
-		bb.rewind();
-
-		item.reset();
-		item.readFrom(bb);
-
-		Assert.assertEquals("Apple", item.getName());
-	}
-
 	@Test
 	public void testSimple() throws Exception {
-		Item item = new Item("Apple");
-		Simple simple = new Simple(item, "Test", 42.0, new Integer[]{1, 2}, Arrays
+		Simple simple = new Simple("Hello", 42.0, new Integer[]{1, 2}, Arrays
 				.asList(3, 5, 8, 13));
 
-		ByteBuffer bb = ByteBuffer.allocate(1024);
-		simple.writeTo(bb);
-		bb.rewind();
+		byte[] buf = new byte[512];
+		simple.writeTo(buf, 0);
 		simple.reset();
-		simple.readFrom(bb);
+		simple.readFrom(buf, 0);
 
-		Assert.assertEquals("Test", simple.getName());
+		Assert.assertEquals("Hello", simple.getName());
 		Assert.assertEquals((Double) 42.0, simple.getValue());
 		Assert.assertSame(1, simple.getArrAt(0));
 		Assert.assertSame(2, simple.getArrAt(1));
@@ -42,7 +24,6 @@ public class StreamTest {
 		Assert.assertSame(5, simple.getLiAt(1));
 		Assert.assertSame(8, simple.getLiAt(2));
 		Assert.assertSame(13, simple.getLiAt(3));
-		Assert.assertEquals("Apple", simple.getItem().getName());
 	}
 
 }
